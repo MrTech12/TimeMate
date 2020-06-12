682c9544a04c42c099240021358811fe24a35ef1
Sina NS <s.nazarsoroush@student.fontys.nl>
2020-06-12 16:48:57 +0200

    Change: alert for agenda deletion
    * Added alert for successfully deleting an agenda.
    * Added check + alert for when a user tries to delete the {Bijbaan}
    agenda.

--- TimeMate/wwwroot/js/account-settings.js
+++ TimeMate/wwwroot/js/account-settings.js
@@ -10,7 +10,13 @@ $(document).ready(function () {
     });
 
     $("#confirm-deletion").click(function () {
-        SendDeleteRequest();
+        if (CheckAgendaName()) {
+            SendDeleteRequest();
+        }
+        else {
+            alert("Deze agenda mag u niet verwijderen.");
+        }
+
     });
 });
 
@@ -18,6 +24,15 @@ function GetSelectedOptionInformation() {
     selectInput = document.getElementById("agenda-select");
     agendaName = $("#agenda-select :selected").val();
     agendaID = $("#agenda-select :selected").attr("id");
+};
+
+function CheckAgendaName() {
+    if (agendaName == "Bijbaan") {
+        return false;
+    }
+    else {
+        return true;
+    }
 };
 
 function SendDeleteRequest() {
@@ -30,6 +45,7 @@ function SendDeleteRequest() {
         traditional: true,
         success: function (data) {
             selectInput.remove(selectInput.selectedIndex);
+            alert("Agenda succesvol verwijderd.");
         },
         error: function (ts) {
             alert('Een error is ontstaan. Probeer het laten opnieuw a.u.b.');
